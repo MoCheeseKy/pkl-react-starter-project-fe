e6dcd175992ccf3db2f14afa52620b3cad3facf7
laukpindang <ferdiansyahbandung3@gmail.com>
2022-10-14 16:58:07 +0700

    another

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import './Login.scss';
+import { useState, useEffect } from 'react';
 
 const Login = () => {
+    function handleSubmit(e) {
+        
+    }
+    
     return (
-        <div>
-            hai
+        <div className='login'>
+            <div className="form">
+                <h1 className='px-3 py-3'>Login</h1>
+                <form className='px-3 py-4' action="">
+                    <div className='form-group'>
+                        <label htmlFor="">Username</label>
+                        <input
+                        type="text"
+                        className='form-control'
+                        name='username'
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="">Password</label>
+                        <input
+                        type="password"
+                        className='form-control'
+                        name='password'
+                        />
+                    </div>
+                    <div className='sub-btn'>
+                        <button className='btn btn-primary'>Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
