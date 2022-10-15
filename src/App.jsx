bc96207d429334e0681f3feacdd011c1ac5bd6b2
Your NameMoCheeseKy <rifkyking04@gmail.com>
2022-10-15 12:20:17 +0700

    new aja

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import React, {Suspense} from 'react';
-import{Route, Routes} from "react-router-dom";
-import {ListRoutes} from "./routes/index";
+import React, { Suspense } from 'react';
+import { Route, Routes } from "react-router-dom";
+import { ListRoutes } from "./routes/index";
 import './App.scss';
 
 function App() {
-  return(
+  return (
     <Suspense fallback={<>loading...</>}>
       <Routes>
-        {
-          ListRoutes.map((item, key) => (
-            <Route key={key} exact path={item.path} element={<item.component />}/>
-          ))
-        }
-        <Route path="*" element={<>Not Found!</>}/>
+
       </Routes>
     </Suspense>
   );
 }
+
 
 
 export default App;
