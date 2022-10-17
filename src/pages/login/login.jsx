382f16abebabc7446e7d09ba09863021ccff615d
laukpindang <ferdiansyahbandung3@gmail.com>
2022-10-17 11:21:12 +0700

    completing login and register

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,60 +1,76 @@
-import { Button, Form, Input, Radio } from 'antd';
-import React, { useState } from 'react';
-
+import { Button, Form, Input } from 'antd';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  const [form] = Form.useForm();
-  const [formLayout, setFormLayout] = useState('horizontal');
-  const onFormLayoutChange = ({ layout }) => {
-    setFormLayout(layout);
+  const onFinish = (values) => {
+    console.log('Success:', values);
   };
-  const formItemLayout =
-    formLayout === 'horizontal'
-      ? {
-          labelCol: {
-            span: 4,
-          },
-          wrapperCol: {
-            span: 14,
-          },
-        }
-      : null;
-  const buttonItemLayout =
-    formLayout === 'horizontal'
-      ? {
-          wrapperCol: {
-            span: 14,
-            offset: 4,
-          },
-        }
-      : null;
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
-    <Form
-      {...formItemLayout}
-      layout={formLayout}
-      form={form}
-      initialValues={{
-        layout: formLayout,
-      }}
-      onValuesChange={onFormLayoutChange}
-    >
-      <Form.Item label="Form Layout" name="layout">
-        <Radio.Group value={formLayout}>
-          <Radio.Button value="horizontal">Horizontal</Radio.Button>
-          <Radio.Button value="vertical">Vertical</Radio.Button>
-          <Radio.Button value="inline">Inline</Radio.Button>
-        </Radio.Group>
-      </Form.Item>
-      <Form.Item label="Field A">
-        <Input placeholder="input placeholder" />
-      </Form.Item>
-      <Form.Item label="Field B">
-        <Input placeholder="input placeholder" />
-      </Form.Item>
-      <Form.Item {...buttonItemLayout}>
-        <Button type="primary">Submit</Button>
-      </Form.Item>
-    </Form>
+    <div className="Login">
+        <center>
+            <h1>Login</h1>
+        </center>
+        <Form
+        name="basic"
+        labelCol={{
+            span: 7,
+        }}
+        wrapperCol={{
+            span: 10,
+        }}
+        initialValues={{
+            remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        >
+        <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+            {
+                required: true,
+                message: 'Please input your username!',
+            },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+
+        <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+            {
+                required: true,
+                message: 'Please input your password!',
+            },
+            ]}
+        >
+            <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+            wrapperCol={{
+            offset: 7,
+            span: 5,
+            }}
+        >
+            <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+            </Button><br />
+            Don't have an account ? 
+            <Link to={`/register`}>
+                register now!
+            </Link>
+        </Form.Item>
+        </Form>
+    </div>
   );
 };
 export default Login;
