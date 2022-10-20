e5d4be73460a39254db39887a02fba3c35f707fa
laukpindang <ferdiansyahbandung3@gmail.com>
2022-10-20 14:38:12 +0700

    check

--- src/components/footer/foot/foot.jsx
+++ src/components/footer/foot/foot.jsx
@@ -29,8 +29,8 @@ export default function Foot() {
         <div className="foot-content">
           <div className="info">
               <div className="info-head"><h3>GET IN TOUCH</h3></div>
-              <p>IBLAM Kampus A <br /> <nobr>Jl. Kramat Raya No. 25, Senen</nobr><br /><nobr> Jakarta Pusat, Telp (021) 21392851</nobr></p>
-              <p>IBLAM Kampus B <br /> Jl. Raden Sanim No. 99 (Tanah Baru) <br /> Kec. Beji, Kota Depok, Telp (021) 7764310 Fax (021) 7762969</p>
+              <p>IBLAM Kampus A <br />Jl. Kramat Raya No. 25, Senen<br /> Jakarta Pusat, Telp (021) 21392851</p>
+              <p>IBLAM Kampus B <br /> Jl. Raden Sanim No. 99 (Tanah Baru)<br /><norb> Kec. Beji, Kota Depok, Telp (021) 7764310 Fax (021) 7762969</norb></p>
           </div>
           <div className="contact">
               <div className="wa">
