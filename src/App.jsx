acf09e27ad785677773ceba4efaf35ebf4bb1e78
laukpindang <ferdiansyahbandung3@gmail.com>
2022-10-24 09:45:36 +0700

    add link

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/home/home';
 import Login from './pages/login/login';
 import Register from './pages/register/register';
 import Daftar from './pages/daftar/daftar';
-
+import Academic from './pages/academic/academic';
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/register' element={<Register />} />
         <Route exact path='/daftar' element={<Daftar />} />
+        <Route exact path='/academic' element={<Academic />} />
         <Route path="*" element={
         <center>
           <h1>Oops! That page can’t be found.</h1>
