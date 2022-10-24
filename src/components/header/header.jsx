fc779dc98aea235e43164fc0ed5b5417388316bb
Your NameMoCheeseKy <rifkyking04@gmail.com>
2022-10-24 14:29:56 +0700

    asdad

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -83,8 +83,6 @@ export default function Header() {
                     <button><Link to={'/academic'}>Academic</Link></button>
                     <button>Admission</button>
                     <button><Link to={'/e-library'}>E-Library</Link></button>
-                    <button>Library</button>
-                    <button>E-Library</button>
                     <button><Link to={'/library'}>Library</Link></button>
                     <button>Life at Iblam</button>
                     <button>About</button>
