0edd4d2120a8ce5336bf7f6b3e0d22830ccef0c5
laukpindang <ferdiansyahbandung3@gmail.com>
2022-10-25 11:16:48 +0700

    check

--- src/pages/news-event/news.jsx
+++ src/pages/news-event/news.jsx
@@ -7,6 +7,16 @@ import Foot from '../../components/footer/foot/foot'
 
 export default function News() {
   return (
-    <div>News</div>
+    <div className='news'>
+        <Header />
+        <div className="content">
+            <div className="entry">
+                <img src="https://iblam.ac.id/wp-content/uploads/2019/12/Header_26.jpg" alt="" />
+            </div>
+            <div className="container"></div>
+        </div>
+        <Subscribe/>
+        <Foot/>
+    </div>
   )
 }
