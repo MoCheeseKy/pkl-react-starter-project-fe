efa68923ad543aa5056671cd27256113c59bc0b5
laukpindang <ferdiansyahbandung3@gmail.com>
2022-10-28 15:59:52 +0700

    asd

--- src/pages/about/kontak/kontak.jsx
+++ src/pages/about/kontak/kontak.jsx
@@ -19,7 +19,7 @@ export default function Kontak() {
                 </div>
                 <div className="kontak-content">
                     <div className="kontak-content-item">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d126907.60746524946!2d106.7530177!3d-6.2817698!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f449bc26bf09%3A0x3dabae99a930c057!2sSTIH%20IBLAM!5e0!3m2!1sid!2sid!4v1666942758843!5m2!1sid!2sid"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d126907.60746524946!2d106.7530177!3d-6.2817698!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f449bc26bf09%3A0x3dabae99a930c057!2sSTIH%20IBLAM!5e0!3m2!1sid!2sid!4v1666942758843!5m2!1sid!2sid" title="jakarta"></iframe>
                         <h4>Kampus A (Jakarta)</h4>
                         <p>Jl. Kramat Raya No. 25, Jakarta Pusat, Indonesia</p>
                         <p>Telepon. +62 21 2139 2851</p>
@@ -27,7 +27,7 @@ export default function Kontak() {
                         <p>Email. info@iblam.ac.id</p>
                     </div>
                     <div className="kontak-content-item">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7930.203544748152!2d106.80161!3d-6.3808632!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69eeb2d5fbadf3%3A0x75547f606f1fca28!2sSTIH%20IBLAM%20(Kampus%20A%20Depok)!5e0!3m2!1sid!2sid!4v1666943353836!5m2!1sid!2sid"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7930.203544748152!2d106.80161!3d-6.3808632!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69eeb2d5fbadf3%3A0x75547f606f1fca28!2sSTIH%20IBLAM%20(Kampus%20A%20Depok)!5e0!3m2!1sid!2sid!4v1666943353836!5m2!1sid!2sid" title="depok"></iframe>
                         <h4>Kampus B (Depok)</h4>
                         <p>Jl. Raden Sanim No. 99 Tanah Baru, Kec. Beji, Depok, Indonesia</p>
                         <p>Telepon. +62 21 776 4310</p>
