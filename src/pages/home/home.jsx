38060ff855abd3361f425fe452a73681d3e723bf
Your NameMoCheeseKy <rifkyking04@gmail.com>
2022-11-01 11:17:37 +0700

    asdad

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./newsEvent.scss"
+import "./walkTogehter.scss"
+import "./homeAbout.scss"
 
 import Header from "../../components/header/header";
 import Subscribe from "../../components/footer/subscribe/subscribe";
@@ -14,6 +16,63 @@ export default function Home() {
         <main>
             <Header />
 
+
+
+            {/* ======================================================================= */}
+            {/* ==================== Bagian Melangkah bersama Kami ==================== */}
+            {/* ======================================================================= */}
+            <div className="melangkahBersama">
+                <div className="melangkahBersama-tittle">
+                    <div className="icon"></div>
+                    <h2>Melangkah Bersama Iblam</h2>
+                </div>
+                <div className="melangkahBersama-content">
+                    <div className="melangkahBersama-content-item">
+                        <div className="content-image first">
+                            <Link to='/academic/kurikulum'>
+                                <button>Explore now <SwapRightOutlined></SwapRightOutlined></button>
+                            </Link>
+                        </div>
+                        <h5>curriculum</h5>
+                        <p>Pilihan konsentrasi pembelajaran hukum yang luas untuk mempersiapkan karir di dunia global</p>
+                    </div>
+                    <div className="melangkahBersama-content-item">
+                        <div className="content-image second">
+                            <Link to='/login'>
+                                <button>Explore now <SwapRightOutlined></SwapRightOutlined></button>
+                            </Link>
+                        </div>
+                        <h5>apply to iblam</h5>
+                        <p>Ketahui proses pendaftaran untuk program S1 dan S2, ketentuan dan deadline. Daftar dengan mudah secara online</p>
+                    </div>
+                    <div className="melangkahBersama-content-item">
+                        <div className="content-image third">
+                            <Link to='/alumni'>
+                                <button>Explore now <SwapRightOutlined></SwapRightOutlined></button>
+                            </Link>
+                        </div>
+                        <h5>alumni</h5>
+                        <p>Bergabung bersama ribuan alumni IBLAM yang telah sukses meraih mimpinya</p>
+                    </div>
+                </div>
+            </div>
+
+
+            {/* ============================================================ */}
+            {/* ==================== Bagian About Iblam ==================== */}
+            {/* ============================================================ */}
+            <div className="homeAbout">
+                <div className="homeAbout-content">
+                    <h5>about iblam</h5>
+                    <h2>Berkomitmen Membangun Bangsa.</h2>
+                    <p>IBLAM berkomitmen untuk menghasilkan sumber daya yang memiliki integritas di bidang hukum, unggul di tingkat nasional, dan
+                        berdaya saing global</p>
+                    <Link to='/about'><button>More about Iblam <SwapRightOutlined></SwapRightOutlined></button></Link>
+                </div>
+                <div className="youtube">
+                    <iframe src="https://www.youtube.com/embed/ufQmdvO_JaY?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
 
 
             {/* ============================================================== */}
