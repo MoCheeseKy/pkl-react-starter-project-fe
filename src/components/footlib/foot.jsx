7a51e95356ce571405c17066afda253b14d58ab2
laukpindang <ferdiansyahbandung3@gmail.com>
2022-11-04 14:51:31 +0700

    complete head

--- src/components/footlib/foot.jsx
+++ src/components/footlib/foot.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import './foot.scss'
 
 export default function Foot() {
   return (
-    <div>Foot</div>
+    <div className='foot'>Foot</div>
   )
 }
--- src/components/headlib/head.jsx
+++ src/components/headlib/head.jsx
@@ -1,7 +1,84 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './head.scss'
+import { Link } from 'react-router-dom'
+import { Drawer, Divider } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
 
 export default function Head() {
+
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  const navStyle = {
+    color: 'white'
+  }
+
   return (
-    <div>Head</div>
+    <div className="head">
+        <div className="logo">
+            <Link>
+                <img src="https://library.iblam.ac.id/images/default/logo.png" alt="" />
+                <div className="txt">
+                    <h1>IBLAM LIBRARY</h1>
+                </div>
+            </Link>
+        </div>
+        <div className="items-large">
+            <ul>
+                <li><Link>Beranda</Link></li>
+                <li><Link>Informasi</Link></li>
+                <li><Link>Berita</Link></li>
+                <li><Link>Bantuan</Link></li>
+                <li><Link>Pustakawan</Link></li>
+                <li><Link>login</Link></li>
+                <li><Link>Koleksi <br /> Disertasi</Link></li>
+                <li><Link>Koleksi <br /> Skripsi</Link></li>
+                <li><Link>Visitor</Link></li>
+                <li><Link>Penyerahan <br /> Skripsi/Tesis</Link></li>
+                <li><Link>Area <br /> Anggota</Link></li>
+                <li><Link>TL</Link></li>
+            </ul>
+        </div>
+        <div className="items-small">
+            <button onClick={showDrawer}><MenuOutlined /></button>
+            <Drawer
+            title="Nav"
+            placement='top'
+            onClose={onClose}
+            bodyStyle={{backgroundColor: '#2D3748'}}
+            open={open}>
+                <Link style={navStyle}>Beranda</Link>
+                <Divider/>
+                <Link style={navStyle}>Informasi</Link>
+                <Divider/>
+                <Link style={navStyle}>Berita</Link>
+                <Divider/>
+                <Link style={navStyle}>Bantuan</Link>
+                <Divider/>
+                <Link style={navStyle}>Pustakawan</Link>
+                <Divider/>
+                <Link style={navStyle}>login</Link>
+                <Divider/>
+                <Link style={navStyle}>Koleksi Disertasi</Link>
+                <Divider/>
+                <Link style={navStyle}>Koleksi Skripsi</Link>
+                <Divider/>
+                <Link style={navStyle}>Visitor</Link>
+                <Divider/>
+                <Link style={navStyle}>Penyerahan Skripsi/Tesis</Link>
+                <Divider/>
+                <Link style={navStyle}>Area Anggota</Link>
+                <Divider/>
+                <Link style={navStyle}>TL</Link>
+            </Drawer>
+        </div>
+    </div>
   )
 }
