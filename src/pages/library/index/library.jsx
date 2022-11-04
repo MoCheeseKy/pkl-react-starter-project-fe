6bf7d9c60c12c1a4e6507dd0584ae73d049e3139
laukpindang <ferdiansyahbandung3@gmail.com>
2022-11-04 10:56:44 +0700

    add head and foot

--- src/pages/library/index/library.jsx
+++ src/pages/library/index/library.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Head from '../../../components/headlib/head'
+import Foot from '../../../components/footlib/foot'
 
 export default function Index() {
   return (
-    <div>Index</div>
+    <div className='index'>
+      <Head/>
+      <Foot/>
+    </div>
   )
 }
