fc8b8d9e268d482f9e225ce4e406f913e7ad04ab
laukpindang <ferdiansyahbandung3@gmail.com>
2022-11-07 13:16:07 +0700

    add link

--- src/components/headlib/head.jsx
+++ src/components/headlib/head.jsx
@@ -50,17 +50,17 @@ export default function Head() {
           </div>
           <div className="items-large">
           <ul>
-            <li><Link>Beranda</Link></li>
-            <li><Link>Informasi</Link></li>
-            <li><Link>Berita</Link></li>
-            <li><Link>Bantuan</Link></li>
-            <li><Link>Pustakawan</Link></li>
-            <li><Link>login</Link></li>
+            <li><Link to={'/library/beranda'}>Beranda</Link></li>
+            <li><Link to={'/library/information'}>Informasi</Link></li>
+            <li><Link to={'/library/berita'}>Berita</Link></li>
+            <li><Link to={'/library/help'}>Bantuan</Link></li>
+            <li><Link to={'/library/pustakawan'}>Pustakawan</Link></li>
+            <li><Link to={'/library/login'}>login</Link></li>
             <li><Link>Koleksi <br /> Disertasi</Link></li>
             <li><Link>Koleksi <br /> Skripsi</Link></li>
-            <li><Link>Visitor</Link></li>
-            <li><Link>Penyerahan <br /> Skripsi/Tesis</Link></li>
-            <li><Link>Area <br /> Anggota</Link></li>
+            <li><Link to={'/library/visitor'}>Visitor</Link></li>
+            <li><Link to={'/library/skripsi'}>Penyerahan <br /> Skripsi/Tesis</Link></li>
+            <li><Link to={'/library/anggota'}>Area <br /> Anggota</Link></li>
             <li><Link>TL</Link></li>
           </ul>
           </div>
@@ -72,27 +72,27 @@ export default function Head() {
           onClose={onClose}
           bodyStyle={{backgroundColor: '#2D3748'}}
           open={open}>
-            <Link style={navStyle}>Beranda</Link>
+            <Link to={'/library/beranda'} style={navStyle}>Beranda</Link>
             <Divider/>
-            <Link style={navStyle}>Informasi</Link>
+            <Link to={'/library/information'} style={navStyle}>Informasi</Link>
             <Divider/>
-            <Link style={navStyle}>Berita</Link>
+            <Link to={'/library/berita'} style={navStyle}>Berita</Link>
             <Divider/>
-            <Link style={navStyle}>Bantuan</Link>
+            <Link to={'/library/help'} style={navStyle}>Bantuan</Link>
             <Divider/>
-            <Link style={navStyle}>Pustakawan</Link>
+            <Link to={'/library/pustakawan'} style={navStyle}>Pustakawan</Link>
             <Divider/>
-            <Link style={navStyle}>login</Link>
+            <Link to={'/library/login'} style={navStyle}>login</Link>
             <Divider/>
             <Link style={navStyle}>Koleksi Disertasi</Link>
             <Divider/>
             <Link style={navStyle}>Koleksi Skripsi</Link>
             <Divider/>
-            <Link style={navStyle}>Visitor</Link>
+            <Link to={'/library/visitor'} style={navStyle}>Visitor</Link>
             <Divider/>
-            <Link style={navStyle}>Penyerahan Skripsi/Tesis</Link>
+            <Link to={'/library/skripsi'} style={navStyle}>Penyerahan Skripsi/Tesis</Link>
             <Divider/>
-            <Link style={navStyle}>Area Anggota</Link>
+            <Link to={'/library/anggota'} style={navStyle}>Area Anggota</Link>
             <Divider/>
             <Link style={navStyle}>TL</Link>
           </Drawer>
