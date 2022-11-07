e6bf63f57cd8179acf69a9e15847059d2e6d65e6
laukpindang <ferdiansyahbandung3@gmail.com>
2022-11-07 11:14:50 +0700

    complete

--- src/components/headlib/head.jsx
+++ src/components/headlib/head.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import './head.scss'
 import { Link } from 'react-router-dom'
-import { Drawer, Divider } from 'antd';
+import { Drawer, Divider, Carousel } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
 
 export default function Head() {
@@ -22,62 +22,74 @@ export default function Head() {
 
   return (
     <div className="head">
-      <div className="logo">
-        <Link>
-          <img src="https://library.iblam.ac.id/images/default/logo.png" alt="" />
-          <div className="txt">
-            <h1>IBLAM LIBRARY</h1>
+      <div className="cover">
+        <Carousel autoplay dots={false}>
+          <div className="img">
+            <img src="https://iblam.ac.id/wp-content/uploads/2019/11/big-banner1.jpg" alt="" />
           </div>
-        </Link>
-      </div>
-      <div className="items-large">
-        <ul>
-          <li><Link>Beranda</Link></li>
-          <li><Link>Informasi</Link></li>
-          <li><Link>Berita</Link></li>
-          <li><Link>Bantuan</Link></li>
-          <li><Link>Pustakawan</Link></li>
-          <li><Link>login</Link></li>
-          <li><Link>Koleksi <br /> Disertasi</Link></li>
-          <li><Link>Koleksi <br /> Skripsi</Link></li>
-          <li><Link>Visitor</Link></li>
-          <li><Link>Penyerahan <br /> Skripsi/Tesis</Link></li>
-          <li><Link>Area <br /> Anggota</Link></li>
-          <li><Link>TL</Link></li>
-        </ul>
-      </div>
-      <div className="items-small">
-        <button onClick={showDrawer}><MenuOutlined /></button>
-        <Drawer
-        title="Nav"
-        placement='top'
-        onClose={onClose}
-        bodyStyle={{backgroundColor: '#2D3748'}}
-        open={open}>
-          <Link style={navStyle}>Beranda</Link>
-          <Divider/>
-          <Link style={navStyle}>Informasi</Link>
-          <Divider/>
-          <Link style={navStyle}>Berita</Link>
-          <Divider/>
-          <Link style={navStyle}>Bantuan</Link>
-          <Divider/>
-          <Link style={navStyle}>Pustakawan</Link>
-          <Divider/>
-          <Link style={navStyle}>login</Link>
-          <Divider/>
-          <Link style={navStyle}>Koleksi Disertasi</Link>
-          <Divider/>
-          <Link style={navStyle}>Koleksi Skripsi</Link>
-          <Divider/>
-          <Link style={navStyle}>Visitor</Link>
-          <Divider/>
-          <Link style={navStyle}>Penyerahan Skripsi/Tesis</Link>
-          <Divider/>
-          <Link style={navStyle}>Area Anggota</Link>
-          <Divider/>
-          <Link style={navStyle}>TL</Link>
-        </Drawer>
+          <div className="img">
+            <img src="https://iblam.ac.id/wp-content/uploads/2019/11/big-banner2.jpg" alt="" />
+          </div>
+        </Carousel>
+        <div className="content">
+          <div className="logo">
+            <Link>
+              <img src="https://library.iblam.ac.id/images/default/logo.png" alt="" />
+              <div className="txt">
+                <h1>IBLAM LIBRARY</h1>
+              </div>
+            </Link>
+          </div>
+          <div className="items-large">
+          <ul>
+            <li><Link>Beranda</Link></li>
+            <li><Link>Informasi</Link></li>
+            <li><Link>Berita</Link></li>
+            <li><Link>Bantuan</Link></li>
+            <li><Link>Pustakawan</Link></li>
+            <li><Link>login</Link></li>
+            <li><Link>Koleksi <br /> Disertasi</Link></li>
+            <li><Link>Koleksi <br /> Skripsi</Link></li>
+            <li><Link>Visitor</Link></li>
+            <li><Link>Penyerahan <br /> Skripsi/Tesis</Link></li>
+            <li><Link>Area <br /> Anggota</Link></li>
+            <li><Link>TL</Link></li>
+          </ul>
+          </div>
+          <div className="items-small">
+          <button onClick={showDrawer}><MenuOutlined /></button>
+          <Drawer
+          title="Nav"
+          placement='top'
+          onClose={onClose}
+          bodyStyle={{backgroundColor: '#2D3748'}}
+          open={open}>
+            <Link style={navStyle}>Beranda</Link>
+            <Divider/>
+            <Link style={navStyle}>Informasi</Link>
+            <Divider/>
+            <Link style={navStyle}>Berita</Link>
+            <Divider/>
+            <Link style={navStyle}>Bantuan</Link>
+            <Divider/>
+            <Link style={navStyle}>Pustakawan</Link>
+            <Divider/>
+            <Link style={navStyle}>login</Link>
+            <Divider/>
+            <Link style={navStyle}>Koleksi Disertasi</Link>
+            <Divider/>
+            <Link style={navStyle}>Koleksi Skripsi</Link>
+            <Divider/>
+            <Link style={navStyle}>Visitor</Link>
+            <Divider/>
+            <Link style={navStyle}>Penyerahan Skripsi/Tesis</Link>
+            <Divider/>
+            <Link style={navStyle}>Area Anggota</Link>
+            <Divider/>
+            <Link style={navStyle}>TL</Link>
+          </Drawer>
+          </div>
+        </div>
       </div>
     </div>
   )
